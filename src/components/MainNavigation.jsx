a6747863a3f70d1a30ee0e7799ca209c83915dc2
abedm <abedmohammed353@gmail.com>
2023-04-06 08:51:21 -0400

    Added animated logo

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,33 +1,39 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import f1logo from "../assets/images/f1logo.png";
+
 const MainNavigation = () => {
   const activeLinkClass = ({ isActive }) => {
     return isActive
-      ? "navigation__links navigation__links--active"
-      : "navigation__links";
+      ? "navigation__link navigation__link--active"
+      : "navigation__link";
   };
 
   return (
     <nav className="navigation">
-      <NavLink to="/" className={activeLinkClass} end>
-        Home
+      <NavLink to="/" end>
+        <div className="navigation__logo">
+          <span role="img" aria-label="Formula 1 logo link to home page"></span>
+        </div>
       </NavLink>
-      <NavLink to="/news" className={activeLinkClass} end>
-        News
-      </NavLink>
-      <NavLink to="/standings" className={activeLinkClass} end>
-        Standings
-      </NavLink>
-      <NavLink to="/drivers" className={activeLinkClass} end>
-        Drivers
-      </NavLink>
-      <NavLink to="/constructors" className={activeLinkClass} end>
-        Constructors
-      </NavLink>
-      <NavLink to="/races" className={activeLinkClass} end>
-        Races
-      </NavLink>
+      <div className="navigation__links">
+        <NavLink to="/news" className={activeLinkClass} end>
+          News
+        </NavLink>
+        <NavLink to="/standings" className={activeLinkClass} end>
+          Standings
+        </NavLink>
+        <NavLink to="/drivers" className={activeLinkClass} end>
+          Drivers
+        </NavLink>
+        <NavLink to="/constructors" className={activeLinkClass} end>
+          Constructors
+        </NavLink>
+        <NavLink to="/races" className={activeLinkClass} end>
+          Races
+        </NavLink>
+      </div>
     </nav>
   );
 };

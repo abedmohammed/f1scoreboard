7e5743b97ffff73d4172cb7c19073bfe73117cc6
abedm <abedmohammed353@gmail.com>
2023-04-14 10:24:03 -0400

    Updated images, added data to driver page

--- src/helpers/extraData.js
+++ src/helpers/extraData.js
@@ -1,107 +1,148 @@
-import VER from "../assets/images/VER.avif";
-import PER from "../assets/images/PER.avif";
-import ALO from "../assets/images/ALO.avif";
-import HAM from "../assets/images/HAM.avif";
-import SAI from "../assets/images/SAI.avif";
-import STR from "../assets/images/STR.avif";
-import RUS from "../assets/images/RUS.avif";
-import NOR from "../assets/images/NOR.avif";
-import HUL from "../assets/images/HUL.avif";
-import LEC from "../assets/images/LEC.avif";
-import BOT from "../assets/images/BOT.avif";
-import OCO from "../assets/images/OCO.avif";
-import PIA from "../assets/images/PIA.avif";
-import GAS from "../assets/images/GAS.avif";
-import ZHO from "../assets/images/ZHO.avif";
-import TSU from "../assets/images/TSU.avif";
-import MAG from "../assets/images/MAG.avif";
-import ALB from "../assets/images/ALB.avif";
-import SAR from "../assets/images/SAR.avif";
-import DEV from "../assets/images/DEV.avif";
+import VER from "../assets/images/headshots/VER.avif";
+import PER from "../assets/images/headshots/PER.avif";
+import ALO from "../assets/images/headshots/ALO.avif";
+import HAM from "../assets/images/headshots/HAM.avif";
+import SAI from "../assets/images/headshots/SAI.avif";
+import STR from "../assets/images/headshots/STR.avif";
+import RUS from "../assets/images/headshots/RUS.avif";
+import NOR from "../assets/images/headshots/NOR.avif";
+import HUL from "../assets/images/headshots/HUL.avif";
+import LEC from "../assets/images/headshots/LEC.avif";
+import BOT from "../assets/images/headshots/BOT.avif";
+import OCO from "../assets/images/headshots/OCO.avif";
+import PIA from "../assets/images/headshots/PIA.avif";
+import GAS from "../assets/images/headshots/GAS.avif";
+import ZHO from "../assets/images/headshots/ZHO.avif";
+import TSU from "../assets/images/headshots/TSU.avif";
+import MAG from "../assets/images/headshots/MAG.avif";
+import ALB from "../assets/images/headshots/ALB.avif";
+import SAR from "../assets/images/headshots/SAR.avif";
+import DEV from "../assets/images/headshots/DEV.avif";
 
-import alfaImg from "../assets/images/alfa.png";
-import alphatauriImg from "../assets/images/alphatauri.png";
-import alpineImg from "../assets/images/alpine.png";
-import aston_martinImg from "../assets/images/aston_martin.png";
-import ferrariImg from "../assets/images/ferrari.png";
-import haasImg from "../assets/images/haas.png";
-import mclarenImg from "../assets/images/mclaren.png";
-import mercedesImg from "../assets/images/mercedes.png";
-import red_bullImg from "../assets/images/red_bull.png";
-import williamsImg from "../assets/images/williams.png";
+import VERFull from "../assets/images/fullbody/VERPortrait.avif";
+import PERFull from "../assets/images/fullbody/PERPortrait.avif";
+import ALOFull from "../assets/images/fullbody/ALOPortrait.avif";
+import HAMFull from "../assets/images/fullbody/HAMPortrait.avif";
+import SAIFull from "../assets/images/fullbody/SAIPortrait.avif";
+import STRFull from "../assets/images/fullbody/STRPortrait.avif";
+import RUSFull from "../assets/images/fullbody/RUSPortrait.avif";
+import NORFull from "../assets/images/fullbody/NORPortrait.avif";
+import HULFull from "../assets/images/fullbody/HULPortrait.avif";
+import LECFull from "../assets/images/fullbody/LECPortrait.avif";
+import BOTFull from "../assets/images/fullbody/BOTPortrait.avif";
+import OCOFull from "../assets/images/fullbody/OCOPortrait.avif";
+import PIAFull from "../assets/images/fullbody/PIAPortrait.avif";
+import GASFull from "../assets/images/fullbody/GASPortrait.avif";
+import ZHOFull from "../assets/images/fullbody/ZHOPortrait.avif";
+import TSUFull from "../assets/images/fullbody/TSUPortrait.avif";
+import MAGFull from "../assets/images/fullbody/MAGPortrait.avif";
+import ALBFull from "../assets/images/fullbody/ALBPortrait.avif";
+import SARFull from "../assets/images/fullbody/SARPortrait.avif";
+import DEVFull from "../assets/images/fullbody/DEVPortrait.avif";
 
-import williamsCar from "../assets/images/williamsCar.avif";
-import alfaCar from "../assets/images/alfaCar.avif";
-import alphatauriCar from "../assets/images/alphatauriCar.avif";
-import alpineCar from "../assets/images/alpineCar.avif";
-import aston_martinCar from "../assets/images/aston_martinCar.avif";
-import ferrariCar from "../assets/images/ferrariCar.avif";
-import haasCar from "../assets/images/haasCar.avif";
-import mclarenCar from "../assets/images/mclarenCar.avif";
-import mercedesCar from "../assets/images/mercedesCar.avif";
-import red_bullCar from "../assets/images/red_bullCar.avif";
+import alfaImg from "../assets/images/logos/alfa.png";
+import alphatauriImg from "../assets/images/logos/alphatauri.png";
+import alpineImg from "../assets/images/logos/alpine.png";
+import aston_martinImg from "../assets/images/logos/aston_martin.png";
+import ferrariImg from "../assets/images/logos/ferrari.png";
+import haasImg from "../assets/images/logos/haas.png";
+import mclarenImg from "../assets/images/logos/mclaren.png";
+import mercedesImg from "../assets/images/logos/mercedes.png";
+import red_bullImg from "../assets/images/logos/red_bull.png";
+import williamsImg from "../assets/images/logos/williams.png";
+
+import williamsCar from "../assets/images/cars/williamsCar.avif";
+import alfaCar from "../assets/images/cars/alfaCar.avif";
+import alphatauriCar from "../assets/images/cars/alphatauriCar.avif";
+import alpineCar from "../assets/images/cars/alpineCar.avif";
+import aston_martinCar from "../assets/images/cars/aston_martinCar.avif";
+import ferrariCar from "../assets/images/cars/ferrariCar.avif";
+import haasCar from "../assets/images/cars/haasCar.avif";
+import mclarenCar from "../assets/images/cars/mclarenCar.avif";
+import mercedesCar from "../assets/images/cars/mercedesCar.avif";
+import red_bullCar from "../assets/images/cars/red_bullCar.avif";
 
 export const driversExtra = {
   VER: {
     image: VER,
+    fullBodyImage: VERFull,
     number: 1,
   },
   PER: {
     image: PER,
+    fullBodyImage: PERFull,
   },
   ALO: {
     image: ALO,
+    fullBodyImage: ALOFull,
   },
   HAM: {
     image: HAM,
+    fullBodyImage: HAMFull,
   },
   SAI: {
     image: SAI,
+    fullBodyImage: SAIFull,
   },
   STR: {
     image: STR,
+    fullBodyImage: STRFull,
   },
   RUS: {
     image: RUS,
+    fullBodyImage: RUSFull,
   },
   NOR: {
     image: NOR,
+    fullBodyImage: NORFull,
   },
   HUL: {
     image: HUL,
+    fullBodyImage: HULFull,
   },
   LEC: {
     image: LEC,
+    fullBodyImage: LECFull,
   },
   BOT: {
     image: BOT,
+    fullBodyImage: BOTFull,
   },
   OCO: {
     image: OCO,
+    fullBodyImage: OCOFull,
   },
   PIA: {
     image: PIA,
+    fullBodyImage: PIAFull,
   },
   GAS: {
     image: GAS,
+    fullBodyImage: GASFull,
   },
   ZHO: {
     image: ZHO,
+    fullBodyImage: ZHOFull,
   },
   TSU: {
     image: TSU,
+    fullBodyImage: TSUFull,
   },
   MAG: {
     image: MAG,
+    fullBodyImage: MAGFull,
   },
   ALB: {
     image: ALB,
+    fullBodyImage: ALBFull,
   },
   SAR: {
     image: SAR,
+    fullBodyImage: SARFull,
   },
   DEV: {
     image: DEV,
+    fullBodyImage: DEVFull,
   },
 };
 
--- src/pages/DriverPage.jsx
+++ src/pages/DriverPage.jsx
@@ -5,9 +5,10 @@ import {
   useRouteLoaderData,
   useParams,
 } from "react-router-dom";
+import PageWrapper from "../components/PageWrapper";
 import { CountriesContext } from "../context/CountriesContext";
 
-import { driversExtra } from "../helpers/extraData";
+import { driversExtra, constructorsExtra } from "../helpers/extraData";
 
 const DriverPage = () => {
   const { getFlag, getNationality } = useContext(CountriesContext);
@@ -15,6 +16,34 @@ const DriverPage = () => {
   const wikiData = useLoaderData().parse.text;
   const data = useRouteLoaderData("drivers");
   const params = useParams();
+
+  const getInfoboxData = (term) => {
+    const startStr = `${term}</th><td class="infobox-data">`;
+    const pos = wikiData.indexOf(startStr) + startStr.length;
+
+    if (wikiData.indexOf(startStr) === -1) {
+      const startStr = `${term}</a></th><td class="infobox-data">`;
+      const pos = wikiData.indexOf(startStr) + startStr.length;
+
+      if (term === "Wins") {
+        const wins = wikiData.substring(
+          pos,
+          wikiData.indexOf("</td></tr><tr>", pos)
+        );
+        if (Number.isNaN(+wins)) {
+          return wikiData
+            .substring(pos, wikiData.indexOf("</td></tr><tr>", pos))
+            .substring(wins.indexOf(">") + 1, wins.lastIndexOf("<"));
+        }
+      }
+
+      return wikiData
+        .substring(pos, wikiData.indexOf("</td></tr><tr>", pos))
+        .substring(">" + 1);
+    }
+
+    return wikiData.substring(pos, wikiData.indexOf("</td>", pos));
+  };
 
   const driverData =
     data.MRData.StandingsTable.StandingsLists[0].DriverStandings.filter(
@@ -27,41 +56,118 @@ const DriverPage = () => {
         );
       }
     )[0];
-
+  console.log(data);
   const year = data.MRData.StandingsTable.season;
   const driver = {
     name: `${driverData.Driver.givenName} ${driverData.Driver.familyName}`,
-    birthDay: driverData.Driver.dataOfBirth,
-    number:
-      driversExtra[driverData.Driver.code]?.number ||
-      driverData.Driver.permanentNumber,
-    position: driverData.position,
-    team: driverData.Constructors[0].name,
-    points: driverData.points,
+    birthDay: driverData.Driver.dateOfBirth,
     nationality: driverData.Driver.nationality,
     country: getNationality(driverData.Driver.nationality)?.country,
     flag: getFlag(getNationality(driverData.Driver.nationality)?.country),
     image: driversExtra[driverData.Driver.code]?.image,
+    fullImage: driversExtra[driverData.Driver.code]?.fullBodyImage,
+    team: driverData.Constructors[0].name,
+    teamLogo: constructorsExtra[driverData.Constructors[0].constructorId]?.logo,
+    position: +driverData.position,
+    points: +driverData.points,
+    number:
+      driversExtra[driverData.Driver.code]?.number ||
+      driverData.Driver.permanentNumber,
+    totalRaces: +getInfoboxData("Entries").split(" ")[0],
+    championships: getInfoboxData("Championships")
+      .split(" (<a")
+      .map((entry, i) => {
+        if (i === 0) {
+          return entry;
+        }
+
+        return entry.split("</a>, <a").map((championship) => {
+          const pos = championship.indexOf('">') + '">'.length;
+          return championship.substring(pos, pos + 4);
+        });
+      }),
+    totalPodiums: +getInfoboxData("Podiums"),
+    totalWins: +getInfoboxData("Wins"),
+    careerPoints: +getInfoboxData("Career points"),
+    polePositions: +getInfoboxData("Pole positions"),
+    fastestLaps: +getInfoboxData("Fastest laps"),
   };
+  console.log(driver);
 
-  console.log(wikiData);
-  const podiums = wikiData.substring(
-    wikiData.indexOf("Podiums") + 1,
-    wikiData.lastIndexOf("Career points")
+  return (
+    <PageWrapper className="driver" title={driver.name}>
+      <div className="driver__container">
+        <div className="driver__image">
+          <img src={driver.fullImage} alt={`${driver.name} standing`} />
+        </div>
+        <div className="driver__content">
+          <div className="driver__header">
+            <div className="driver__team">
+              <h3>{driver.team}</h3>
+              <img src={driver.teamLogo} alt="" />
+            </div>
+            <div className="driver__flag">
+              <img src={driver.flag} alt={`Flag of ${driver.country}`} />
+            </div>
+          </div>
+          <div className="stats">
+            <ul>
+              <li className="stats__row">
+                <p className="stats__title">Birth Date:</p>
+                <p className="stats__data">
+                  {new Intl.DateTimeFormat("en-US", {
+                    month: "long",
+                    day: "numeric",
+                    year: "numeric",
+                  }).format(new Date(driver.birthDay))}
+                </p>
+              </li>
+              <li className="stats__row">
+                <p className="stats__title">Country:</p>
+                <p className="stats__data">{driver.country}</p>
+              </li>
+              <li className="stats__row">
+                <p className="stats__title">Car Number:</p>
+                <p className="stats__data">{driver.number}</p>
+              </li>
+              <li className="stats__row">
+                <p className="stats__title">Grand Prix Entered:</p>
+                <p className="stats__data">{driver.totalRaces}</p>
+              </li>
+              <li className="stats__row">
+                <p className="stats__title">Career Points:</p>
+                <p className="stats__data">{driver.careerPoints}</p>
+              </li>
+              <li className="stats__row">
+                <p className="stats__title">Total Wins:</p>
+                <p className="stats__data">{driver.totalWins}</p>
+              </li>
+              <li className="stats__row">
+                <p className="stats__title">Total Podiums:</p>
+                <p className="stats__data">{driver.totalPodiums}</p>
+              </li>
+              <li className="stats__row">
+                <p className="stats__title">Fastest Laps:</p>
+                <p className="stats__data">{driver.fastestLaps}</p>
+              </li>
+              <li className="stats__row">
+                <p className="stats__title">Pole Positions:</p>
+                <p className="stats__data">{driver.polePositions}</p>
+              </li>
+              <li className="stats__row">
+                <p className="stats__title">{year} Points:</p>
+                <p className="stats__data">{driver.points}</p>
+              </li>
+              <li className="stats__row">
+                <p className="stats__title">{year} Position:</p>
+                <p className="stats__data">{driver.position}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </PageWrapper>
   );
-  console.log(podiums);
-
-  // DATA
-  // born
-  // races
-  // championships
-  // wins
-  // podiums
-  // career points
-  // pole positions
-  // fastest laps
-
-  return <div>DriverPage</div>;
 };
 
 export default DriverPage;
@@ -74,7 +180,6 @@ export async function loader({ params }) {
       driverName = "Carlos_Sainz_Jr.";
       break;
     case "George_Russell":
-      console.log("Caught!");
       driverName = "George_Russell_(racing_driver)";
       break;
     default:

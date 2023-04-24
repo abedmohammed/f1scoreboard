aad8bcea73bb9127d6fad61d43b1e23d4179fabb
abedm <abedmohammed353@gmail.com>
2023-04-24 14:31:47 -0400

    Adding copyright not in use

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -2,10 +2,22 @@ import React from "react";
 
 const PageWrapper = ({ className, title, children }) => {
   return (
-    <main className="page">
-      {title && <h2 className="page__title">{title}</h2>}
-      <div className={className}>{children}</div>
-    </main>
+    <>
+      <main className="page">
+        {title && <h2 className="page__title">{title}</h2>}
+        <div className={className}>{children}</div>
+      </main>
+      {/* <p className="page__copyright">
+        &copy; Copyright {new Date().getFullYear()}&nbsp;|&nbsp;
+        <a
+          href="https://github.com/abedmohammed"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Mohammed Abed
+        </a>
+      </p> */}
+    </>
   );
 };
 
